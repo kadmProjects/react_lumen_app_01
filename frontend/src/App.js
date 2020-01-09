66864f8f86ddc3df9bf108a6a8a32a4b01c15980
kadmProjects <40564817+kadmProjects@users.noreply.github.com>
2020-01-09 13:41:09 +0530

    Added basic structure for the application. Added top navigation and side navigation bar.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,201 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import clsx from 'clsx';
+import { fade, makeStyles } from '@material-ui/core/styles';
+import InputBase from '@material-ui/core/InputBase';
+import Drawer from '@material-ui/core/Drawer';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import List from '@material-ui/core/List';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import MenuIcon from '@material-ui/icons/Menu';
+import DashboardIcon from '@material-ui/icons/Dashboard';
+import MovieIcon from '@material-ui/icons/Movie';
+import RecentActorsIcon from '@material-ui/icons/RecentActors';
+import SupervisedUserCircleIcon from '@material-ui/icons/SupervisedUserCircle';
+import BusinessIcon from '@material-ui/icons/Business';
+import LocationCityIcon from '@material-ui/icons/LocationCity';
+import SearchIcon from '@material-ui/icons/Search';
 
-function App() {
+const drawerWidth = 240;
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        display: 'flex',
+        flexGrow: 1,
+    },
+    appBar: {
+        zIndex: theme.zIndex.drawer + 1,
+        transition: theme.transitions.create(['width', 'margin'], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+        }),
+    },
+    menuButton: {
+        marginRight: 36,
+    },
+    hide: {
+        display: 'none',
+    },
+    drawer: {
+        width: drawerWidth,
+        flexShrink: 0,
+        whiteSpace: 'nowrap',
+    },
+    drawerPaper: {
+        top: 64,
+    },
+    drawerOpen: {
+        width: drawerWidth,
+        transition: theme.transitions.create('width', {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+    },
+    drawerClose: {
+        transition: theme.transitions.create('width', {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+        }),
+        overflowX: 'hidden',
+        width: theme.spacing(7) + 1,
+        [theme.breakpoints.up('sm')]: {
+            width: theme.spacing(7) + 1,
+        },
+    },
+    toolbar: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'flex-end',
+        padding: theme.spacing(0, 1),
+        ...theme.mixins.toolbar,
+    },
+    content: {
+        flexGrow: 1,
+        padding: theme.spacing(3),
+    },
+    title: {
+        flexGrow: 1,
+        display: 'none',
+        [theme.breakpoints.up('sm')]: {
+            display: 'block',
+        },
+    },
+    search: {
+        position: 'relative',
+        borderRadius: theme.shape.borderRadius,
+        backgroundColor: fade(theme.palette.common.white, 0.15),
+        '&:hover': {
+          backgroundColor: fade(theme.palette.common.white, 0.25),
+        },
+        marginLeft: 0,
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+          marginLeft: theme.spacing(1),
+          width: 'auto',
+        },
+    },
+    searchIcon: {
+        width: theme.spacing(7),
+        height: '100%',
+        position: 'absolute',
+        pointerEvents: 'none',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    inputRoot: {
+        color: 'inherit',
+    },
+    inputInput: {
+        padding: theme.spacing(1, 1, 1, 7),
+        transition: theme.transitions.create('width'),
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+          width: 120,
+          '&:focus': {
+            width: 200,
+          },
+        },
+    },
+    backdrop: {
+        zIndex: theme.zIndex.drawer + 1,
+        color: '#fff',
+    },
+}));
+
+export default function App() {
+    const classes = useStyles();
+    const [open, setOpen] = React.useState(false);
+
+    const toggleDrawer = () => {
+        setOpen(!open);
+    };
+
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>Edit <code>src/App.js</code> and save to reload.</p>
-                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">Learn React</a>
-            </header>
+        <div className={classes.root}>
+            <CssBaseline />
+            <AppBar position="fixed" className={ clsx(classes.appBar) } >
+                <Toolbar>
+                    <IconButton color="inherit" aria-label="open drawer" onClick={toggleDrawer} edge="start" className={ clsx(classes.menuButton) } >
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography className={classes.title} variant="h6" noWrap>Mini variant drawer</Typography>
+                    <div className={classes.search}>
+                        <div className={classes.searchIcon}>
+                            <SearchIcon />
+                        </div>
+                        <InputBase placeholder="Search…" classes={{ root: classes.inputRoot, input: classes.inputInput, }} inputProps={{ 'aria-label': 'search' }} />
+                    </div>
+                </Toolbar>
+            </AppBar>
+            <Drawer variant="permanent" className={clsx(classes.drawer, { [classes.drawerOpen]: open, [classes.drawerClose]: !open, })} classes={{ paper: clsx(classes.drawerPaper, { [classes.drawerOpen]: open, [classes.drawerClose]: !open, }), }} >
+                <List>
+                    <ListItem button key='Dashboard'>
+                        <ListItemIcon>
+                            <DashboardIcon />
+                        </ListItemIcon>
+                        <ListItemText primary='Dashboard' />
+                    </ListItem>
+                    <ListItem button key='Movies'>
+                        <ListItemIcon>
+                            <MovieIcon />
+                        </ListItemIcon>
+                        <ListItemText primary='Movies' />
+                    </ListItem>
+                    <ListItem button key='Actors'>
+                        <ListItemIcon>
+                            <RecentActorsIcon />
+                        </ListItemIcon>
+                        <ListItemText primary='Actors' />
+                    </ListItem>
+                    <ListItem button key='Directors'>
+                        <ListItemIcon>
+                            <SupervisedUserCircleIcon />
+                        </ListItemIcon>
+                        <ListItemText primary='Directors' />
+                    </ListItem>
+                    <ListItem button key='Production Compnay'>
+                        <ListItemIcon>
+                            <BusinessIcon />
+                        </ListItemIcon>
+                        <ListItemText primary='Production Compnay' />
+                    </ListItem>
+                    <ListItem button key='Home Town'>
+                        <ListItemIcon>
+                            <LocationCityIcon />
+                        </ListItemIcon>
+                        <ListItemText primary='Home Town' />
+                    </ListItem>
+                </List>
+            </Drawer>
+            <main className={classes.content}>
+                <div className={classes.toolbar} />
+            </main>
         </div>
-    );
+  );
 }
-
-export default App;
